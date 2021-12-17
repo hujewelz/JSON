c287839add142dc3f34db09e3a9a34b9d892a5da
Jewelz <hujewelz@163.com>
2021-12-17 11:39:40 +0800

    Test JSON

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -108,3 +108,42 @@ extension JSON {
         return nil
     }
 }
+
+func jsonData() -> Data? {
+    let jsonStr = """
+     [
+         {
+             "name": "Jewelz",
+             "email": "example@gmil.com",
+             "address": {
+                 "street": "reai road.",
+                 "city": "Zhe Jiang"
+             }
+         },
+         {
+             "name": "Tom",
+             "email": "tomcat@gmil.com",
+             "address": {
+                 "street": "big mouse road.",
+                 "city": "An Hui"
+             }
+         },
+         {
+             "name": "Jo",
+             "email": "jo@gmil.com",
+             "address": {
+                 "street": "Joo road.",
+                 "city": "Shang Hai"
+             }
+         }
+     ]
+    """
+    return jsonStr.data(using: .utf8)
+}
+
+if let data = jsonData(), let json = JSON(data: data) {
+    let name = json[0]?["name"]?.stringValue
+    print("name: ", name)
+    let city = json[0]?["address"]?["city"]?.stringValue
+    print("city: ", city)
+}
